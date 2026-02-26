33369befe541ba1abfdc789b020767288bc2d1f5
Zo <161946731+zozo0329@users.noreply.github.com>
2026-02-26 16:04:22 +0800

    all fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainHeader from "./Components/HeaderContent/MainHeader/MainHeader";
 import Main from "./Components/MainContent/Main/MainFiles/Main";
+import MainFooter from "./Components/FooterContent/MainFooter/MainFooter";
 // import CartProvider from "./Store/CartProvider";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <MainHeader />
       <Main />
+      <MainFooter />
     </div>
   );
 };
---
+++ src/Components/FooterContent/MainFooter/MainFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Content from "../Content/Content";
+
+const MainFooter = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+};
+
+export default MainFooter;
